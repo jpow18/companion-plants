5d16707bf391da888b92ac3316c059fd7e70af73
James Pow <jbpow18@icloud.com>
2023-01-27 12:56:54 -0500

    First Commit.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Companion Planting Advice</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+  <?php include("./view/header.php") ?>
+  
+  <ul id="sorted-list"></ul>
+  <footer>
+    <p>Contact Us: contact@example.com</p>
+    <p>Copyright © 2022 Example Company</p>
+  </footer>
+</body>
+
+</html>
---
+++ view/form.php
@@ -0,0 +1,6 @@
+<form>
+  <label for="plant-input">Enter plants (separated by commas):</label>
+  <input type="text" id="plant-input" placeholder="broccoli, carrots, etc.">
+  <input type="submit" value="Submit">
+  <input type="reset" value="Reset">
+</form>
