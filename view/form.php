caa10bfac86ed09d0276d83f8704bd1c306e6772
James Pow <jbpow18@icloud.com>
2023-01-29 17:40:45 -0500

    Changed default text in form.

--- view/form.php
+++ view/form.php
@@ -1,6 +1,6 @@
 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="GET">
   <label for="plant-input">Enter plant</label>
-  <input type="text" name="plant" id="plant-input" pattern="[a-zA-Z]+" title=" Only alphabetical characters are allowed" placeholder="broccoli, carrots, etc.">
+  <input type="text" name="plant" id="plant-input" pattern="[a-zA-Z]+" title=" Only alphabetical characters are allowed" placeholder="broccoli, carrot, etc.">
   <input type="submit" value="Submit">
   <input type="reset" value="Reset" onclick="window.location.href='<?php echo $_SERVER['PHP_SELF']; ?>'">
 </form>
